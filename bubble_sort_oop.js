1d04f71994a4a1c03190f227250f846ebcf60b03
Aj <adtune6509@student.waynecc.edu>
2019-04-24 15:37:00 -0400

    Update bubble_sort_oop.js

--- bubble_sort_oop.js
+++ bubble_sort_oop.js
@@ -1,15 +1,11 @@
 'use strict'
-
 let myArr = [28, 6, 5, 42, 20, 12, 2, 48, 3, 33, 31, 44, 1, 22, 25, 13, 16, 39, 37, 49, 4, 35, 10, 14, 26, 38, 24];
-
 Array.prototype.swap = function(i, j){
   let temp = this[j]
   this[j] = this[i]
   this[i] = temp
   
 }
-var list = [2,1,3,6,7]
-console.log(list)
 Array.prototype.bubble_sort = function() {
   for(let k = 0; k < this.length; k++) {
     for(let j = 0; j < this.length - k; j++){
@@ -21,15 +17,4 @@ Array.prototype.bubble_sort = function() {
   return this;
   
 }
-var b = function(arr){
-  for (let i = 0; i < arr.length; i++){
-    for(let j = 0; j < arr.length - i; i++){
-      if(arr[j] > arr[j+1]){
-        arr.swap(j+1, j)
-      }
-    }
-  }
-  return arr
-}
-
-console.warn(myArr.bubble_sort())
+console.log(myArr.bubble_sort())
